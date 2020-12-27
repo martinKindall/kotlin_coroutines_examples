293210a85509467215cea51fb6b531a768723db2
martinKindall <mart256@gmail.com>
2020-12-27 19:42:26 +0100

    Added more examples from Basics of Coroutines

--- src/main/kotlin/org/example/Hello.kt
+++ src/main/kotlin/org/example/Hello.kt
@@ -1,8 +1,6 @@
 package org.example
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 // runBlocking doesnt complete until
 // every coroutine launched in its scope
@@ -10,9 +8,45 @@ import kotlinx.coroutines.runBlocking
 fun main() = runBlocking<Unit> {
 
     launch {
-        delay(1000L)
-        println("World!")
+        emitMsg()
     }
 
-    println("Hello ")
+    coroutineScope {
+        launch {
+            delay(2000L)
+            println("Task from nested launch")
+        }
+
+        delay(1000L)
+        println("Task from Coroutine Scope")
+    }
+
+    println("Coroutine Scope is over")
+    launchManyIsSafe()
+    globalScopeIsLikeADaemonThread()
+}
+
+suspend fun emitMsg() {
+    delay(500L)
+    println("Task from runBlocking")
+}
+
+suspend fun launchManyIsSafe() = coroutineScope {
+    repeat(100_000) {
+        launch {
+            delay(2000L)
+            print(".")
+        }
+    }
+}
+
+suspend fun globalScopeIsLikeADaemonThread() {
+    GlobalScope.launch {
+        repeat(1000) { i ->
+            println("Im sleeping $i...")
+            delay(500L)
+        }
+    }
+
+    delay(1300L)
 }
