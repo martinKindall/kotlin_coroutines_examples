f9922c570aeb7fea94d631a8832ef87a62028870
martinKindall <mart256@gmail.com>
2020-12-27 19:00:33 +0100

    Not using GlobalScope

--- src/main/kotlin/org/example/Hello.kt
+++ src/main/kotlin/org/example/Hello.kt
@@ -1,25 +1,18 @@
 package org.example
 
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlin.concurrent.thread
 
+// runBlocking doesnt complete until
+// every coroutine launched in its scope
+// is completed
 fun main() = runBlocking<Unit> {
 
-    GlobalScope.launch {
+    launch {
         delay(1000L)
         println("World!")
     }
 
-    thread {
-        Thread.sleep(500L)
-        println("Hi from other thread!")
-    }
-
     println("Hello ")
-//    Thread.sleep(2000L) // block main thread to keen JVM alive
-    delay(2000L)
-    println("Learning Coroutines in Kotlin")
 }
